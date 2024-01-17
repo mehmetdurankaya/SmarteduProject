da2fa36892d8e9a6e0a98755e6d0dd77c3227e08
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2024-01-17 21:32:34 +0300

    second commit

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require("express");
 
 const app = express();
 
+//Teplate Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
 app.get("/", (req, res) => {
-  res.status(200).send("Index Sayfası");
+  res.status(200).render('index',{
+    page_name:'index'
+  });
 });
+app.get('/about', (req, res) => {
+  res.status(200).render('about',{
+    page_name:'about'
+  });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
